a5940cbdbb1d76d5c942f0c640da10d913ad4229
Ilona Sidarovich <ilona_sidarovich@epam.com>
2018-03-06 18:38:49 +0300

    broken test for controller

--- src/main/java/org/student/unit/task/controller/HealthCheckController.java
+++ src/main/java/org/student/unit/task/controller/HealthCheckController.java
@@ -2,9 +2,11 @@ package org.student.unit.task.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
